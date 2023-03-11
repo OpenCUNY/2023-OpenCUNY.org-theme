eebdb53e6ef7329dec653e836851cd66a35b51fd
hieburt <31567843+hieburt@users.noreply.github.com>
2023-03-10 19:41:56 -0500

    Create header.php

---
+++ OpenCUNY Homepage/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+  <head>
+  	<!-- Meta -->
+  	<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<meta name="description" content="OpenCUNY.org landing page">
+  	<meta name="author" content="http://opencuny.org">
+  	<meta name="shortcut icon" hreg="images/logo.png">
+
+	<!-- Loads the theme CSS & page title via functions.php -->
+		<?php
+			wp_head();
+		?>
+  </head>
+<div class="site-banner">
+  		<h1 class="site-title">Open*CUNY</h1>
+</div>
+
+<body>
