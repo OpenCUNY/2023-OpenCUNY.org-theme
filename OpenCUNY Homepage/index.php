99d4ccc2d8fd1376dcf92893ddaee772f0f2ba28
hieburt <31567843+hieburt@users.noreply.github.com>
2023-03-10 19:42:39 -0500

    Create index.php

---
+++ OpenCUNY Homepage/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+	<title>Blog Site Template</title>
+
+	<!-- Meta -->
+
+	<meta charset="utf-8">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<meta name="description" content="Blog Site Template">
+
+	<meta name="author" content="https://opencuny.org">
+
+
+
+	<!-- Theme CSS -->
+
+	<?php
+
+	wp_head();
+
+	?>
+
+</head>
+
+<body>
+
+  	<div class="site-banner">
+
+  		<h1 class="site-title">Open*CUNY</h1>
+
+ 	</div>
+
+ 	<div class="login-form">
+
+  		<p>here</p>
+
+ 	</div>
+
+
+
+  	<div class="body-content">
+
+  		<button class="accordion pink">What is OpenCUNY? ▾</button>
+
+		<div class="panel">
+
+   			<p><em>OpenCUNY is a student-run organization that serves students at the Graduate Center, CUNY, and is sponsored by the Doctoral and Graduate Students Council (DGSC).</em></p>
+
+    		<p>Since our founding by Greg Donovan in 2008, we’ve hosted OpenCUNY.org as a digital community and WordPress platform for GC students. Each semester we convene conferences and informational events about open-source software and data privacy.</p>
+
+    		<p>We’re always exploring ways to make new digital tools available to GC students that will help us to research, teach, organize, and commune.</p>
+
+		</div>
+
+  		<button class="accordion green">How is OpenCUNY Unique? ▾</button>
+
+		<div class="panel">
+
+			<p><em>OpenCUNY helps you keep control of your own work.</em></p>
+
+			<p>Data ownership and copyright are complex issues in the digital world and especially academic spaces. Because OpenCUNY.org is not hosted on school servers and does not fall under CUNY administrator’s control, OpenCUNY.org offers a unique, CUNY-affiliated, student-centered space to teach and organize.</p>
+
+			<p>Additionally, we make every effort to protect your digital rights (see our terms of service). You can feel safe hosting your personal, professional, or organization’s data on this platform.</p>
+
+			<p><em>OpenCUNY is student-run and student-funded.</em></p>
+
+			<p>OpenCUNY is the only WordPress platform at the GC run by students for students. We’re funded by DGSC and coordinators work with a DGSC-elected board of OpenCUNY users to develop and improve the platform annually.</p>
+
+			<p><em>Using free and open source software is a political and ethical choice.</em></p>
+
+			<p>Free software is accessible and open source software is free to study, customize, and distribute. OpenCUNY uses the open source version of WordPress, and works to find open source plugins and solutions whenever possible in an effort to protect the digital rights of students.</p>
+
+		</div>
+
+		<button class="accordion yellow">Who can I talk to? ▾</button>
+
+		<div class="panel">
+
+			<p>We're particpant-driven, so we try our best to respond to requests for ideas, help, themes, plugins, etc.</p>
+
+			<p>The quickest way to get in touch with someone is to email <a href="mailto:info@opencuny.org">info@opencuny.org</a>, but you can also email any of the coordinators individually (our email addresses are listed below).</p>
+
+			<h3>Coordinators</h3>
+
+		</div>
+
+	</div>
+
+
+
+	<div class="footer">
+
+		<hr/>
+
+		<p><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" class="float-left" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"/></a>Creative Commons (CC) license unless otherwise noted.</p>
+
+	</div>
+
+	
+
+ </body>
+
+ </html>
