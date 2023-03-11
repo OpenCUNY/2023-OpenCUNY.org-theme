d28dc428738dae29cb8f2446829f02bf86c2bc1b
hieburt <31567843+hieburt@users.noreply.github.com>
2023-03-10 19:40:24 -0500

    Create footer.php

---
+++ OpenCUNY Homepage/footer.php
@@ -0,0 +1,10 @@
+<!-- loads scripts including accordian.js-->
+<?php
+  wp_footer();
+?>
+
+<div class="footer">
+	<hr/>
+	<p><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" class="float-left" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a> OpenCUNY <script>document.write(new Date().getFullYear())</script> | Affiliate of the <a rel="author" href="https://cunydsc.org">DGSC</a>, The Graduate Center, CUNY | Read our <a href="https://opencuny.org/terms/">Terms of Participation</a></p>
+</div>
+</html>
