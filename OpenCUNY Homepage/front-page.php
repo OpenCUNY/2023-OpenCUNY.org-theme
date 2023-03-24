478cb0e6f3c126ed8fddd6fb51cf5c1d1fe4da66
hieburt <31567843+hieburt@users.noreply.github.com>
2023-03-24 09:15:36 -0400

    Quick fixes
    - moved css to styles.css
    - fixed <ol> tag
    - fixed spacing

--- OpenCUNY Homepage/front-page.php
+++ OpenCUNY Homepage/front-page.php
@@ -29,29 +29,20 @@
 		<p>Free software is accessible and open-source software is free to study, customize, and distribute. OpenCUNY uses the open-source version of WordPress, and works to find open-source plugins and solutions whenever possible in an effort to protect the digital rights of students.</p>
 	</div>
 	<button class="accordion yellow">How do I use OpenCUNY? ▾</button>
-<div class="panel lt-blue">
-  <p><em>OpenCUNY can be used to support your research, projects,  events, student organizations, and more.</em></p>
-  <p>Plus, you can create a site with OpenCUNY.org in a matter of minutes:</p>
-  <o>
-    <li><a href="https://opencuny.org/wp-login">Sign up</a> for an account and verify your GC email to confirm it.</li>
-    <li>Return here to sign in using your OpenCUNY.org credentials.</li>
-    <li>Navigate to <a href="https://opencuny.org/wp-login">Create site</a> and complete the form to add a new site to your account.</li>
-  </ul>
-  <p>See our <a href="https://opencuny.info">knowledge base</a> for further guidance on how to customize, improve, and maintain your site.</p>
-  <p><em>Have a question or feature request in mind?</em></p>
-  <p>As a community-led platform, we try our best to respond to requests for help and support as well as for themes, plugins, and other open-source solutions.</p>
-  <p>Contact us at <a href="mailto:info@opencuny.org">info@opencuny.org</a> or visit our office hours on Wednesday from 3-5pm in Room 5396.</p> 
-  <p><a href="https://opencuny.org/coordinators">Read here</a> to learn more about OpenCUNY coordinators</p>
-</div>
-
-<style>
-  .lt-blue li {
-    list-style: circle;
-    margin-left: 20px;
-    padding: 5px;
-  }
-</style>
-	
+	<div class="panel lt-blue">
+	  <p><em>OpenCUNY can be used to support your research, projects, events, organizations, and more.</em></p>
+	  <p>Plus, you can create a site with OpenCUNY.org in a matter of minutes:</p>
+		<ol>
+		 	<li><a href="https://opencuny.org/wp-login">Sign up</a> for an account and verify your GC email to confirm it.</li>
+		 	<li>Return here to sign in using your OpenCUNY.org credentials.</li>
+		 	<li>Navigate to <a href="https://opencuny.org/wp-login" >Create site</a> and complete the form to add a new site to your account.</li>
+		</ol>
+	  <p>See our <a href="https://opencuny.info" aria-label="Search our knowledge base">knowledge base</a> for further guidance on how to customize, improve, and maintain your site.</p>
+	  <p><em>Have a question or feature request in mind?</em></p>
+	  <p>As a community-led platform, we try our best to respond to requests for help and support as well as for themes, plugins, and other open-source solutions.</p>
+	  <p>Contact us at <a href="mailto:info@opencuny.org">info@opencuny.org</a> or visit our office hours on Wednesday from 3-5pm in Room 5396.</p> 
+	  <p><a href="https://opencuny.org/coordinators">Read here</a> to learn more about OpenCUNY coordinators</p>
+	</div>
 </div>
 <?php
 	// retrieves footer.php
