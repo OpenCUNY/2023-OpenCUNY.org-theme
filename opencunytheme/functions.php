29c9da2c9129b38a738516c79df71844c7c9fc79
hieburt <31567843+hieburt@users.noreply.github.com>
2023-09-22 10:19:40 -0400

    moved style.css
    styles.css is required to be in the root directory by WP

--- opencunytheme/functions.php
+++ opencunytheme/functions.php
@@ -13,7 +13,7 @@
 	function opencuny_register_styles(){
 		// Insert most up-to-date CSS file into page headers
 		$version = wp_get_theme()->get( 'Version' );
-		wp_enqueue_style('opencuny-styles', get_template_directory_uri() . "/assets/css/style.css", array(), $version, 'all');
+		wp_enqueue_style('opencuny-styles', get_template_directory_uri() . "/style.css", array(), $version, 'all');
 	}
 
 	add_action( 'wp_enqueue_scripts', 'opencuny_register_styles');
