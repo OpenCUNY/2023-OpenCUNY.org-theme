bf62b547c697588c9fe098a2e86df0f0f457245e
hieburt <31567843+hieburt@users.noreply.github.com>
2023-09-22 09:10:25 -0400

    Update 404.php
    Updated image paths

--- OpenCUNY Homepage/404.php
+++ OpenCUNY Homepage/404.php
@@ -3,7 +3,7 @@
 ?>
 <div class="container">
 <div>
-	<img class="cat" id="cornelius" src="<?php echo esc_url( get_template_directory_uri() . 'assets/images/cat.png' ); ?>" alt="Cornelius the cat"></img>
+	<img class="cat" id="cornelius" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/cat.png' ); ?>" alt="Cornelius the cat"></img>
 	<div class="hand-drawn centered lt-pink">
 		<h1 class="centered single-spaced">Something's not right…</h1>
 		<p class="centered em">Search OpenCUNY or <a href="https://opencuny.org">return to the homepage</a></p>
@@ -12,7 +12,7 @@
 			<input type="submit" class="search-submit" value="Search">
 		</form>
 	</div>
-	<img class="flower-pot" src="<?php echo esc_url( get_template_directory_uri() . 'assets/images/flower-pot.png' ); ?>" alt="Broken flower pot"></img>
+	<img class="flower-pot" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/flower-pot.png' ); ?>" alt="Broken flower pot"></img>
 </div>
 </div>
 <?php
