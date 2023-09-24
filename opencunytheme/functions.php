cf2cbab695380fe5826820c4edb33037e1aea974
hieburt <31567843+hieburt@users.noreply.github.com>
2023-09-24 15:24:33 -0400

    Working concept for welcome page slideshow
    - added create QR codes image
    - added slideshow.js (from W3 website)
    - added slideshow styles to css
    - reorganized functions.php (with help from ChatGPT)
    - removed redundant image folder

--- opencunytheme/functions.php
+++ opencunytheme/functions.php
@@ -1,47 +1,44 @@
 <?php
     
     
-    function opencuny_theme_support(){
-        // Adds dynamic page title support
-        add_theme_support('title-tag');
-        // Adds featured image to post edit screen
-        add_theme_support('post_thumbnails');
-    }
-
-    add_action('after_setup_theme', 'opencuny_theme_support');
-
-    function opencuny_register_styles(){
-        // Insert most up-to-date CSS file into page headers
-        $version = wp_get_theme()->get( 'Version' );
-        wp_enqueue_style('opencuny-styles', get_template_directory_uri() . "/style.css", array(), $version, 'all');
-    }
-
-    add_action( 'wp_enqueue_scripts', 'opencuny_register_styles');
-
-    if ( ! function_exists( 'opencuny_register_nav_menu' ) ) {
-        // calls user-created header and footer menus
-        function opencuny_register_nav_menu(){
-            register_nav_menus( array(
-            'footer-menu' => __( 'Footer Menu', 'text_domain' ),
-            'header-menu' => __( 'Header Menu', 'text_domain' ),
-        ) );        }
-    }
-    
-    add_action( 'after_setup_theme', 'opencuny_register_nav_menu', 0 );
-
-
-    function opencuny_accordian_script() {
-        // inserts the accordian.js into the footer so accordians can be used on any page (e.g. frontpage.php)
-        wp_enqueue_script('opencuny-accordian', get_template_directory_uri() . '/assets/js/accordian.js', array(), '1.0', true);
-    }
-    
-    add_action( 'wp_enqueue_scripts', 'opencuny_accordian_script');
-
-// This loads the QR code generator script on pages with the qr-code templaste
-function qr_code_generator() {
-  if ( is_page_template( 'qr-code.php' ) ) {
-    wp_enqueue_script( 'qr_generator_js', get_template_directory_uri() . '/assets/js/qrcode.js' );
-    wp_enqueue_script( 'qr_form_js', get_template_directory_uri() . '/assets/js/qrcode-form.js' );
-  }
+    // Add theme support for dynamic page titles and featured images
+function opencuny_theme_support() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
 }
-add_action( 'wp_head', 'qr_code_generator' );
+add_action('after_setup_theme', 'opencuny_theme_support');
+
+// Register and enqueue styles
+function opencuny_register_styles() {
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('opencuny-styles', get_template_directory_uri() . "/style.css", array(), $version, 'all');
+}
+add_action('wp_enqueue_scripts', 'opencuny_register_styles');
+
+// Register header and footer menus
+function opencuny_register_nav_menu() {
+    register_nav_menus(array(
+        'footer-menu' => __('Footer Menu', 'text_domain'),
+        'header-menu' => __('Header Menu', 'text_domain'),
+    ));
+}
+add_action('after_setup_theme', 'opencuny_register_nav_menu', 0);
+
+// Enqueue the accordion script
+function opencuny_accordion_script() {
+    wp_enqueue_script('opencuny-accordion', get_template_directory_uri() . '/assets/js/accordion.js', array(), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'opencuny_accordion_script');
+
+// Enqueue scripts based on page templates
+function load_scripts_based_on_template() {
+    if (is_page_template('qr-code.php')) {
+        wp_enqueue_script('qr_generator_js', get_template_directory_uri() . '/assets/js/qrcode.js');
+        wp_enqueue_script('qr_form_js', get_template_directory_uri() . '/assets/js/qrcode-form.js');
+    } 
+}
+add_action('wp_head', 'load_scripts_based_on_template');
+
+
+
+
--- opencunytheme/template-parts/content-welcome.php
+++ opencunytheme/template-parts/content-welcome.php
@@ -1,6 +1,39 @@
 <div class="welcome-message">
 	<h1>Welcome back!</h1>
-	<img id="edgar" alt="Edgar the cat" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/edgar.png' ); ?>">
+<!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img alt="Create QR codes" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/qrcode-slide.png' ); ?>">
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img id="edgar" alt="Edgar the cat" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/flower-pot.png' ); ?>">
+    <div class="caption-text">Caption Two</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img id="edgar" alt="Edgar the cat" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/edgar.png' ); ?>">
+    <div class="captioni-text">Caption Three</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div> 
+<script src="<?php echo esc_url( get_template_directory_uri() . '/assets/js/slideshow.js' ); ?> "></script>
 	<nav>
 		<a href="<?php echo admin_url(); ?>" title="Go to Dashboard">Dashboard</a> |
 		<a href="<?php echo wp_registration_url(); ?>" title="Create new site">Create site</a> |
